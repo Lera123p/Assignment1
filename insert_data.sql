de02d5468935ad518bdd395b94250aae9522739c
Lera123p <vmezhevych@kse.org.ua>
2025-05-20 17:44:59 +0300

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,37 @@
+INSERT INTO EMPLOYEES VALUES
+(1, 'Valeriia'),
+(2, 'Alice'),
+(3, 'Lina'),
+(4, 'Nina'),
+(5, 'Oleksandr');
+
+INSERT INTO PRODUCTS VALUES
+(1, 'Laptop', 25, 40000.00),
+(2, 'Smartphone', 40, 20000.00),
+(3, 'Tablet', 15, 17000.00),
+(4, 'Earphones', 35, 3000.00),
+(5, 'Keyboard', 20, 8000.00);
+
+
+
+INSERT INTO SALES VALUES
+(6, 15),
+(7, 30),
+(8, 10),
+(9, 22),
+(10, 12);
+
+INSERT INTO SALARIES VALUES
+(1, 30000.00),
+(2, 20000.00),
+(3, 10000.00),
+(4, 35000.00),
+(5, 40000.00);
+
+
+INSERT INTO TRANSACTIONS (transaction_id, employee_id, product_id, sale_id, quantity) VALUES
+(1, 1, 1, 1, 3),
+(2, 2, 2, 2, 5),
+(3, 3, 3, 3, 7),
+(4, 4, 4, 4, 10),
+(5, 5, 5, 5, 13);
